0ace07e76320eafff497940c3a58aa621c7cc025
Param Patel <101729325+Paramp24@users.noreply.github.com>
2024-07-19 12:13:31 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,12 +117,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'main/static'),
 ]
+
+
+## WHY IS STATIC FIL NOT LOADING
