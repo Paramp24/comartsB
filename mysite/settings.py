c70c4f87777255cdf306705b54fb9ce019759a26
Param Patel <101729325+Paramp24@users.noreply.github.com>
2024-07-19 17:31:21 -0400

    jklj
    jlkjlkjlkjklj

--- mysite/settings.py
+++ mysite/settings.py
@@ -50,8 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 
 ]
 
@@ -121,8 +119,4 @@ USE_TZ = True
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  ##  the path to directory where collectstatic will collect 
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
