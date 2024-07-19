76b01f00ed98fa390573facaf688356ce027c632
Param Patel <101729325+Paramp24@users.noreply.github.com>
2024-07-19 17:19:30 -0400

    df
    fssf

--- mysite/settings.py
+++ mysite/settings.py
@@ -117,4 +117,14 @@ USE_I18N = True
 USE_TZ = True
 
 
+
+if not DEBUG:
+    from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  ##  the path to directory where collectstatic will collect 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
