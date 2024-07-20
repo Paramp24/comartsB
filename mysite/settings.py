05a6073e2cdd9656b0120e6c9890c2444ddd59fb
Param Patel <101729325+Paramp24@users.noreply.github.com>
2024-07-20 15:32:08 -0400

    j

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,4 +128,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'main/static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build/static')
