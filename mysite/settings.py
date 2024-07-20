1586cd7da70b4cbc2297003de14b2a79b3c04371
Param Patel <101729325+Paramp24@users.noreply.github.com>
2024-07-20 12:46:34 -0400

    f
    f

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,7 +120,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 STATIC_URL = '/static/'
 
-#STATICFILES_DIRS = os.path.join(BASE_DIR, 'main/static'),
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'main/static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # dont delete the top 3 lines they cannot be the proble
