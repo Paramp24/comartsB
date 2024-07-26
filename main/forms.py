9103c6f0afe4464e7ebbc3df7c0ad349c117f3c6
Param Patel <101729325+Paramp24@users.noreply.github.com>
2024-07-26 18:14:44 -0400

    k

--- main/forms.py
+++ main/forms.py
@@ -5,14 +5,37 @@ from django.contrib.auth.models import User
 from .models import ServiceRequest, BusinessProfile, Profile, Review
 
 class SignUpForm(forms.ModelForm):
-    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Password'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password Confirmation'}))
-    account_type = forms.ChoiceField(choices=[('individual', 'Individual'), ('business', 'Business')])
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'placeholder': 'Email'
+        })
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Password'
+        })
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Password Confirmation'
+        })
+    )
+    account_type = forms.ChoiceField(
+        choices=[('individual', 'Individual'), ('business', 'Business')],
+        widget=forms.Select(attrs={
+            'class': 'select_option',
+        })
+    )
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', 'account_type')
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Username',
+            })
+        }
 
     def save(self, commit=True):
         user = super().save(commit=False)
